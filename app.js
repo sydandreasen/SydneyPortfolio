ce2377f5be7c27334355b28643157fa05710f665
sydandreasen <sydandreasen@abbnebraska.com>
2020-10-19 20:06:43 -0500

    add some basic animations to pictures and home section

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
-window.addEventListener('load', () => {
-    if (window.innerWidth < 700)
-    {
-        // have on mobile mode
-        document.getElementById("nav").classList.add("navBar-mobile");
-        document.getElementById("nav").classList.remove("navBar-wide");
-    }
-    // shouldn't need else because defaults to wide mode
-})
+window.addEventListener("load", () => {
+  if (window.innerWidth < 700) {
+    // have on mobile mode
+    document.getElementById("nav").classList.add("navBar-mobile");
+    document.getElementById("nav").classList.remove("navBar-wide");
+  }
+  // shouldn't need else because defaults to wide mode
+});
 
-window.addEventListener('resize', () =>{
-    if (window.innerWidth < 700)
-    {
-        // mobile mode
-        document.getElementById("nav").classList.add("navBar-mobile");
-        document.getElementById("nav").classList.remove("navBar-wide");
-    }
-    else{
-        // wide mode
-        document.getElementById("nav").classList.remove("navBar-mobile");
-        document.getElementById("nav").classList.add("navBar-wide");
-    }
-})
+window.addEventListener("resize", () => {
+  if (window.innerWidth < 700) {
+    // mobile mode
+    document.getElementById("nav").classList.add("navBar-mobile");
+    document.getElementById("nav").classList.remove("navBar-wide");
+  } else {
+    // wide mode
+    document.getElementById("nav").classList.remove("navBar-mobile");
+    document.getElementById("nav").classList.add("navBar-wide");
+  }
+});
+
+let home = document.getElementById("home");
+let about = document.getElementById("about");
+let projects = document.getElementById("projects");
+let internship = document.getElementById("internship");
+let research = document.getElementById("research");
